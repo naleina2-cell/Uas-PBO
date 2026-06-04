53f769fdb5beb2302959227a0f646dd0b2a741ba
unknown <naleina2@gmail.com>
2026-06-04 19:24:16 +0800

    Initial project structure

---
+++ src/character/Tank.java
@@ -0,0 +1,5 @@
+package character;
+
+public class Tank extends Hero {
+
+}
---
+++ src/main/MainGame.java
@@ -0,0 +1,9 @@
+package main;
+
+public class MainGame {
+
+    public static void main(String[] args) {
+        System.out.println("MOBA 3v3");
+    }
+
+}
